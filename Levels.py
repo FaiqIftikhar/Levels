74588825be7c4e386a1ccfe67e6774e9c2bfc7cb
Faiq Iftikhar <faiqiftikhar1@gmail.com>
2024-07-31 23:35:14 +0200

    Code refactoring

--- Levels.py
+++ Levels.py
@@ -1,3 +1,8 @@
+"""
+Module providing the main landing page of the app.
+You can start the app by running `py -m streamlit run Levels.py`.
+"""
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -16,9 +21,11 @@ st.sidebar.info("Select a page above.")
 
 st.markdown(
     """
-    This is a small data app that shows salaries in hourly rate across different countries of europe.\n
+    This is a small data app that shows salaries in hourly rate across 
+    different countries of europe.\n
     **👈 You can select one of the views from the sidebar**\n
 
-    Source of all data [Eurostat](https://ec.europa.eu/eurostat/databrowser/view/earn_mw_cur__custom_12336095/default/bar?lang=en).
+    Source of all data 
+    [Eurostat](https://ec.europa.eu/eurostat/databrowser/view/earn_mw_cur__custom_12336095/default/bar?lang=en).
 """
 )
