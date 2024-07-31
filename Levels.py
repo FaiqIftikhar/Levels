6e4afc20c1a976422d84e8951ef78d6464775bc2
Faiq Iftikhar <faiqiftikhar1@gmail.com>
2024-07-31 23:42:58 +0200

    Refactoring

--- Levels.py
+++ Levels.py
@@ -4,28 +4,34 @@ You can start the app by running `py -m streamlit run Levels.py`.
 """
 
 import streamlit as st
-import pandas as pd
-import numpy as np
 from utils import *
 
+class Levels:
 
-st.set_page_config(
-    page_title="Levels",
-    page_icon="💸",
-)
+    def __init__(self) -> None:
+        pass    
+    
+    def setup_mainpage():
+        st.set_page_config(
+            page_title="Levels",
+            page_icon="💸",
+        )
 
-st.title("Levels for your salaries")
+        st.title("Levels for your salaries")
 
-st.sidebar.info("Select a page above.")
+        st.sidebar.info("Select a page above.")
+
+        st.markdown(
+            """
+            This is a small data app that shows salaries in hourly rate across 
+            different countries of europe.\n
+            **👈 You can select one of the views from the sidebar**\n
+
+            Source of all data 
+            [Eurostat]
+            (https://ec.europa.eu/eurostat/databrowser/view/earn_mw_cur__custom_12336095/default/bar?lang=en).
+        """
+        )
 
 
-st.markdown(
-    """
-    This is a small data app that shows salaries in hourly rate across 
-    different countries of europe.\n
-    **👈 You can select one of the views from the sidebar**\n
-
-    Source of all data 
-    [Eurostat](https://ec.europa.eu/eurostat/databrowser/view/earn_mw_cur__custom_12336095/default/bar?lang=en).
-"""
-)
+LEVELS = Levels().setup_mainpage()
